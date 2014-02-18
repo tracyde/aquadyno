3be6705fefa64492d95503e1dbb383295385dcb2
Derek Tracy <tracyde@gmail.com>
2014-02-17 20:01:26 -0500

    added ambient temperature probe via the tmp102 sensor

--- aqsm/probes/probes.go
+++ aqsm/probes/probes.go
@@ -2,6 +2,8 @@ package probes
 
 import (
 	"fmt"
+	"github.com/mrmorphic/hwio"
+	"github.com/mrmorphic/hwio/devices/tmp102"
 	"github.com/tracyde/aquadyno/probe"
 	"github.com/tracyde/onewire"
 )
@@ -9,12 +11,18 @@ import (
 type Probes int
 
 const (
-	ID = "28-000005b3c94a"
+	ID1 = "28-000005b3c94a"
 )
 
 func (*Probes) Gather(_ *struct{}, reply *[]probe.Probe) error {
 	var probes []probe.Probe
-	probes = append(probes, *probe.NewThermal("tank1", "ambient temp", 22.197))
+
+	// Create ambient temp probe
+	aT, err := getAmbient()
+	if err != nil {
+		return err
+	}
+	probes = append(probes, *probe.NewThermal("tank1", "ambient temp", aT))
 
 	names, err := onewire.ScanSlaves()
 	if err != nil {
@@ -43,4 +51,32 @@ func (*Probes) Gather(_ *struct{}, reply *[]probe.Probe) error {
 
 	*reply = probes
 	return nil
+}
+
+func getAmbient() (float32, error) {
+	// Get the i2c module from the driver
+	m, err := hwio.GetModule("i2c")
+	if err != nil {
+		fmt.Printf("Error getting i2c module: %v\n", err)
+		return 0, err
+	}
+
+	// Assert that it is an i2c module
+	i2c := m.(hwio.I2CModule)
+
+	// Enable the i2c bus, needed for Raspberry Pi
+	i2c.Enable()
+	defer i2c.Disable()
+
+	// Get a temp device on this bus
+	temp := tmp102.NewTMP102(i2c)
+
+	// Get the temperature sensor value
+	t, err := temp.GetTemp()
+	if err != nil {
+		fmt.Printf("Error while reading i2c temp: %v\n", err)
+		return 0, err
+	}
+
+	return t, nil
 }
