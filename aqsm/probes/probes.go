41095b631a609be3d28e88298558a75baa676b4b
Derek Tracy <tracyde@gmail.com>
2014-02-23 11:05:05 -0500

    refactored to consolidate probe reading code and to prepare for config manager integration

--- aqsm/probes/probes.go
+++ aqsm/probes/probes.go
@@ -1,82 +1,39 @@
 package probes
 
 import (
-	"fmt"
-	"github.com/mrmorphic/hwio"
-	"github.com/mrmorphic/hwio/devices/tmp102"
 	"github.com/tracyde/aquadyno/probe"
-	"github.com/tracyde/onewire"
 )
 
 type Probes int
 
-const (
-	ID1 = "28-000005b3c94a"
-)
-
-func (*Probes) Gather(_ *struct{}, reply *[]probe.Probe) error {
+func (*Probes) Gather(_ *struct{}, reply *[]probe.Probe) (err error) {
 	var probes []probe.Probe
 
 	// Create ambient temp probe
-	aT, err := getAmbient()
+	ambientProbe := *probe.NewThermal("tank1", "i2c", "0x48", "ambient temp")
+
+	// Read ambient probe data
+	ambientProbe.Value, err = ambientProbe.ReadData()
 	if err != nil {
 		return err
 	}
-	probes = append(probes, *probe.NewThermal("tank1", "ambient temp", aT))
 
-	names, err := onewire.ScanSlaves()
+	// Add ambient probe to probes
+	probes = append(probes, ambientProbe)
+
+	// Create aquarium temp probe
+	aquaProbe := *probe.NewThermal("tank1", "1wire", "28-000005b3c94a", "aquarium temp")
+
+	// Read aquarium temp data
+	aquaProbe.Value, err = aquaProbe.ReadData()
 	if err != nil {
-		fmt.Printf("Error scanning 1wire names: %v", err)
 		return err
 	}
 
-	devices := make([]*onewire.DS18B20, len(names))
-	for i := range names {
-		devices[i], err = onewire.NewDS18B20(names[i])
-		if err != nil {
-			fmt.Printf("Error opening device %v: %v", devices[i], err)
-			return err
-		}
-	}
+	// Add aquarium temp probe to probes
+	probes = append(probes, aquaProbe)
 
-	for i := range devices {
-		fmt.Printf("attempting read on device %x\n", devices[i].Id)
-		value, err := devices[i].Read()
-		if err != nil {
-			fmt.Printf("Error on read: %v", err)
-			return err
-		}
-		probes = append(probes, *probe.NewThermal("tank1", "aquarium temp", float32(value)/1000))
-	}
-
+	// Format reply
 	*reply = probes
 	return nil
-}
-
-func getAmbient() (float32, error) {
-	// Get the i2c module from the driver
-	m, err := hwio.GetModule("i2c")
-	if err != nil {
-		fmt.Printf("Error getting i2c module: %v\n", err)
-		return 0, err
-	}
-
-	// Assert that it is an i2c module
-	i2c := m.(hwio.I2CModule)
-
-	// Enable the i2c bus, needed for Raspberry Pi
-	i2c.Enable()
-	defer i2c.Disable()
-
-	// Get a temp device on this bus
-	temp := tmp102.NewTMP102(i2c)
-
-	// Get the temperature sensor value
-	t, err := temp.GetTemp()
-	if err != nil {
-		fmt.Printf("Error while reading i2c temp: %v\n", err)
-		return 0, err
-	}
-
-	return t, nil
 }
--- probe/probe.go
+++ probe/probe.go
@@ -1,18 +1,103 @@
 package probe
 
+import (
+	"fmt"
+	"github.com/mrmorphic/hwio"
+	"github.com/mrmorphic/hwio/devices/tmp102"
+	"github.com/tracyde/onewire"
+	"log"
+)
+
 type Probe struct {
 	Target string
+	Bus    string
+	Id     string
 	Name   string
-	Value  float32
 	Type   string
+	Value  float32
 }
 
-func NewThermal(target, name string, value float32) *Probe {
+func NewThermal(target, bus, id, name string) *Probe {
 	result := new(Probe)
 	result.Target = target
+	result.Bus = bus
+	result.Id = id
 	result.Name = name
 	result.Type = "temp"
-	result.Value = value
 
 	return result
+}
+
+func (p *Probe) ReadData() (v float32, err error) {
+	// ensure Id is not empty
+	if p.Id == "" {
+		log.Printf("Probe Error: No id:%v for probe named: %v\n", p.Id, p.Name)
+	}
+
+	switch p.Bus {
+	case "i2c":
+		v, err = p.readI2cData()
+	case "1wire":
+		v, err = p.read1WireData()
+	default:
+		err = fmt.Errorf("Unsupported bus type: %v", p.Bus)
+	}
+	return
+}
+
+func (p *Probe) readI2cData() (v float32, err error) {
+	// Get the i2c module from the driver
+	m, err := hwio.GetModule("i2c")
+	if err != nil {
+		log.Printf("Error getting i2c module: %v\n", err)
+		return
+	}
+
+	// Assert that it is an i2c module
+	i2c := m.(hwio.I2CModule)
+
+	// Enable the i2c bus, needed for Raspberry Pi
+	i2c.Enable()
+	defer i2c.Disable()
+
+	// Get a temp device on this bus
+	temp := tmp102.NewTMP102(i2c)
+
+	// Get the temperature sensor value
+	v, err = temp.GetTemp()
+	if err != nil {
+		log.Printf("Error while reading i2c temp: %v\n", err)
+		return
+	}
+
+	return
+}
+
+func (p *Probe) read1WireData() (v float32, err error) {
+	names, err := onewire.ScanSlaves()
+	if err != nil {
+		log.Printf("Error scanning 1wire names: %v", err)
+		return
+	}
+
+	devices := make([]*onewire.DS18B20, len(names))
+	for i := range names {
+		devices[i], err = onewire.NewDS18B20(names[i])
+		if err != nil {
+			log.Printf("Error opening device %v: %v\n", devices[i], err)
+			return
+		}
+	}
+
+	for i := range devices {
+		log.Printf("attempting read on device %x\n", devices[i].Id)
+		value, err := devices[i].Read()
+		if err != nil {
+			log.Printf("Error on read: %v\n", err)
+			return 0, err
+		}
+		v = float32(value) / 1000
+	}
+
+	return
 }
